607a397bde5c3acc822968cdd1c0ec6e24643ec9
Nikolay <mizenko1998@gmail.com>
2019-02-14 01:30:07 +1000

    add travis CI

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-lvl2-s417
-[![asciicast](https://asciinema.org/a/pUosPZkna0TMfPb2YeYXGfspP.svg)](https://asciinema.org/a/pUosPZkna0TMfPb2YeYXGfspP)
+[![Build Status](https://travis-ci.org/NeoShkolnick/project-lvl2-s417.svg?branch=master)](https://travis-ci.org/NeoShkolnick/project-lvl2-s417)
+[![asciicast](https://asciinema.org/a/JD4GXiRCWWUIaAmxKIuQNMQWl.svg)](https://asciinema.org/a/JD4GXiRCWWUIaAmxKIuQNMQWl)
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,6 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('absolute path', () => {
-  const path1 = '/home/nick/hexlet/Project2/__tests__/__fixtures__/after1.json';
-  const path2 = '/home/nick/hexlet/Project2/__tests__/__fixtures__/before1.json';
-  const res = fs.readFileSync('/home/nick/hexlet/Project2/__tests__/__fixtures__/res1', 'utf8');
-  expect(genDiff(path1, path2)).toBe(res);
-});
-
 test('relative path', () => {
   const path1 = './__tests__/__fixtures__/after1.json';
   const path2 = './__tests__/__fixtures__/before1.json';
