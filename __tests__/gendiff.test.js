6d379c46794861bf5a77a9827f48a114cc3598d1
Nikolay <mizenko1998@gmail.com>
2019-02-14 01:39:45 +1000

    corrected tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,7 +4,7 @@ import genDiff from '../src';
 test('relative path', () => {
   const path1 = './__tests__/__fixtures__/after1.json';
   const path2 = './__tests__/__fixtures__/before1.json';
-  const res = fs.readFileSync('/home/nick/hexlet/Project2/__tests__/__fixtures__/res1', 'utf8');
+  const res = fs.readFileSync('./__tests__/__fixtures__/res1', 'utf8');
   expect(genDiff(path1, path2)).toBe(res);
 });
 
