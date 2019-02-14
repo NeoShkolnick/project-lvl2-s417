0b03fbf019b90bbaf00e739d971bdceb1c539036
Nikolay <mizenko1998@gmail.com>
2019-02-14 19:46:08 +1000

    add support for .ini

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 import genDiff from '../src';
 
 const pathToTestFiles = './__tests__/__fixtures__/';
-const pathsToJSON = [pathToTestFiles + 'before.json', pathToTestFiles + 'after.json'];
-const pathsToYML = [pathToTestFiles + 'before.yml', pathToTestFiles + 'after.yml'];
-const pathsToINI = [pathToTestFiles + 'before.ini', pathToTestFiles + 'after.ini'];
+const pathsToJSON = [`${pathToTestFiles}before.json`, `${pathToTestFiles}after.json`];
+const pathsToYML = [`${pathToTestFiles}before.yml`, `${pathToTestFiles}after.yml`];
+const pathsToINI = [`${pathToTestFiles}before.ini`, `${pathToTestFiles}after.ini`];
 
 const res = fs.readFileSync('./__tests__/__fixtures__/res', 'utf8');
 test.each([pathsToJSON, pathsToYML, pathsToINI])(
-  "tests different format(JSON, yml, ini)",
+  'tests different format(JSON, yml, ini)',
   (firsPath, secondPath) => {
     expect(genDiff(firsPath, secondPath)).toBe(res);
-  }
+  },
 );
